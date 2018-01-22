c7bda767567f25b083b83248a397d534d0448e25
lacorp2513 <lizer132@yahoo.com.mx>
2018-01-22 09:19:04 -0600

    classroom and homework folders ready

---
+++ classroom/MyProgramJavaProgram.java
@@ -0,0 +1,8 @@
+public class MyProgramJavaProgram{
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    System.out.println("Posicion 0: "+ args[0]);
+    System.out.println("Posicion 1: "+ args[1]);
+    System.out.println("Posicion 2: "+ args[2]);
+  }
+}
